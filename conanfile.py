5d46211042f36705a58d4c6dccdd7af8ba45d8e7
exqudens <exqudens@gmail.com>
2026-03-28 21:13:44 -0700

    [4-iteration] conan file is done

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from pathlib import Path
 required_conan_version = ">=2.0"
 
 from conan import ConanFile
-from conan.tools.files import copy, save, collect_libs
+from conan.tools.files import copy, collect_libs
 
 class ConanConfiguration(ConanFile):
     settings = "arch", "os", "compiler", "build_type"
@@ -20,48 +20,6 @@ class ConanConfiguration(ConanFile):
     def set_version(self):
         try:
             self.version = Path(__file__).parent.joinpath('name-version.txt').read_text().split(':')[1].strip()
-        except Exception as e:
-            self.output.error(e)
-            raise e
-
-    def requirements(self):
-        try:
-            self.requires("cyclonedds-cxx/0.10.5", transitive_headers=True)
-        except Exception as e:
-            self.output.error(e)
-            raise e
-
-    def generate(self):
-        try:
-            filename: str = 'conan-packages.cmake'
-            content: str = ''
-            cmake_package_name_property: str = 'cmake_file_name'
-
-            content += 'set("${PROJECT_NAME}_CONAN_PACKAGE_NAMES"\n'
-            for dep in self.dependencies.values():
-                content += f'    "{dep.ref.name}"\n'
-            content += ')\n'
-
-            content += 'set("${PROJECT_NAME}_CMAKE_PACKAGE_NAMES"\n'
-            for dep in self.dependencies.values():
-                content += f'    "{dep.cpp_info.get_property(cmake_package_name_property, check_type=str)}" # {dep.ref.name}\n'
-            content += ')\n'
-
-            content += 'set("${PROJECT_NAME}_CMAKE_PACKAGE_VERSIONS"\n'
-            for dep in self.dependencies.values():
-                content += f'    "{dep.ref.version}" # {dep.ref.name}\n'
-            content += ')\n'
-
-            content += 'set("${PROJECT_NAME}_CMAKE_PACKAGE_PATHS"\n'
-            for dep in self.dependencies.values():
-                content += f'    "{Path(dep.package_folder).as_posix()}" # {dep.ref.name}\n'
-            content += ')\n'
-
-            save(self, filename, content)
-
-            for dep in self.dependencies.values():
-                for dir in dep.cpp_info.bindirs:
-                    copy(self, pattern="*", src=dir, dst=Path(self.build_folder).joinpath("bin").as_posix())
         except Exception as e:
             self.output.error(e)
             raise e
